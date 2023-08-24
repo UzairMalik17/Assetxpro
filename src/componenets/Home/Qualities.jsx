83ad27f5d9aecc07198b8370f41d1d589797971c
UzairMalik17 <uzairam17@gmail.com>
2023-08-25 00:48:01 +0500

    Qualities

---
+++ src/componenets/Home/Qualities.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import ComponentWrapper from "../Shared/ComponentWrapper";
+import TradersRow from "./TradersRow";
+
+function Qualities() {
+  const qualities = [
+    {
+      name: "EXPERTS BY YOUR SIDE",
+      img: "box1.png",
+    },
+    {
+      name: "COMMITED TO OUR CLIENTS",
+      img: "box2.png",
+    },
+    {
+      name: "EASY ACCOUNTS OPENING",
+      img: "box3.png",
+    },
+    {
+      name: "SUPPORTS",
+      img: "box4.png",
+    },
+    {
+      name: "BONUS",
+      img: "box5.png",
+    },
+    {
+      name: "RISK MANAGEMENT",
+      img: "box6.png",
+    },
+  ];
+  const toptrades = [
+    {
+      User: "User",
+      Assets: "Assets",
+      Profit: "Profit",
+      Direction: "Direction",
+    },
+    {
+      User: 1,
+      Assets: "AUD/JPY",
+      Profit: "$ 231",
+      Direction: "Call",
+    },
+    {
+      User: 2,
+      Assets: "AUD/JPY",
+      Profit: "$ 231",
+      Direction: "Call",
+    },
+    {
+      User: 3,
+      Assets: "AUD/JPY",
+      Profit: "$ 231",
+      Direction: "Call",
+    },
+    {
+      User: 4,
+      Assets: "AUD/JPY",
+      Profit: "$ 231",
+      Direction: "Call",
+    },
+  ];
+  const toptrends = [
+    {
+      Asset: "Asset",
+      Call: "% Call",
+      Percentage: "Asset Percentage",
+      put: "% Put",
+    },
+    {
+      Asset: "AUD/USD",
+      Call: "52%",
+      Percentage: "Asset Percentage",
+      put: "52%",
+    },
+    {
+      Asset: "AUD/USD",
+      Call: "52%",
+      Percentage: "Asset Percentage",
+      put: "52%",
+    },
+    {
+      Asset: "AUD/USD",
+      Call: "52%",
+      Percentage: "Asset Percentage",
+      put: "52%",
+    },
+    {
+      Asset: "AUD/USD",
+      Call: "52%",
+      Percentage: "Asset Percentage",
+      put: "52%",
+    },
+  ];
+  return (
+    <ComponentWrapper style={`relative`}>
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 bg-black absolute left-0 text-center text-white text-2xl font-extrabold pb-1">
+        {qualities.map((item, index) => {
+          return (
+            <div
+              className={`h-[350px] bg-[url('${item.img}')] bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center `}
+              key={index}>
+              {item.name}
+            </div>
+          );
+        })}
+        <div className="w-full">
+          <div className="w-full h-[65px] bg-green-700 flex justify-center items-center">
+            TOP TRADES
+          </div>
+          <div className="w-full text-black text-base font-semibold bg-white py-8">
+            <table className="w-full">
+              <tbody>
+                {toptrades.map((item, index) => {
+                  return (
+                    <TradersRow
+                      style={`py-4 ${
+                        index === 0 ? "font-bold" : "font-normal"
+                      }`}
+                      key={index}
+                      value1={item.User}
+                      value2={item.Assets}
+                      value3={item.Profit}
+                      value4={item.Direction}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="w-full h-[65px] bg-green-700 flex justify-center items-center">
+            POPULAR TRENDS
+          </div>
+          <div className="w-full text-black text-base font-semibold bg-white py-8">
+            <table className="w-full">
+              <tbody>
+                {toptrends.map((item, index) => {
+                  return (
+                    <TradersRow
+                      style={`py-4 ${
+                        index === 0 ? "font-bold" : "font-normal"
+                      }`}
+                      key={index}
+                      value1={item.Asset}
+                      value2={item.Call}
+                      value3={item.Percentage}
+                      value4={item.put}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="w-full h-[65px] bg-green-700 flex justify-center items-center">
+            FINANCIAL NEWS
+          </div>
+          <div className="w-full h-[calc(100%-65px)] text-black text-base font-semibold bg-white py-8"></div>
+        </div>
+      </div>
+    </ComponentWrapper>
+  );
+}
+
+export default Qualities;
