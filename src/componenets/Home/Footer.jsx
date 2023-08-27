6738c9fe56d4d4255409cec9f27c6153b3a452b4
UzairMalik17 <uzairam17@gmail.com>
2023-08-27 20:27:13 +0500

    Footer

---
+++ src/componenets/Home/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ComponentWrapper from "../Shared/ComponentWrapper";
+
+function Footer() {
+  return (
+    <ComponentWrapper style={`bg-black`}>
+      <div className="w-full flex justify-center items-center text-white opacity-50 text-xl text-center py-4">
+        COPYRIGHT © ASSETXPRO 2020. ALL RIGHTS RESERVED
+      </div>
+    </ComponentWrapper>
+  );
+}
+
+export default Footer;
