d2df4994012320ffccb4eaa8ef30bc264bcc8e93
UzairMalik17 <uzairam17@gmail.com>
2023-09-09 23:43:18 +0500

    Compliance

---
+++ src/componenets/Home/BonusPolicy.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+function BonusPolicy() {
+  return (
+    <div className="w-full flex flex-col gap-8 justify-start item-center md:items-star">
+      <p className="text-3xl font-extrabold">LEVEREGE / BONUS POLICY</p>
+      <p>
+        This Bonus Policy Agreement is entered into by any user who is
+        registered with the company and has an active trading account
+        (hereinafter “the client”, “you” or “trader”). It is clarified and
+        emphasized that the decision whether to grant a bonus to a certain
+        customer is at the Company’s sole and exclusive discretion. The bonus
+        policy hereinafter is subject to the main “Terms of Use” As they are
+        from time to time which can be found at the Company’s website.
+      </p>
+      <ol className="list-decimal pl-8">
+        <li>
+          1. Definition of a trading bonus: A trading bonus is an added value
+          that matches your deposit in your AssetXPro ™ Trading Account and it
+          provides you with more funds to use when you are trading. Trading
+          bonuses come in many forms; there are consistent deposit matches which
+          means that your account will be given an added value when you deposit
+          funds over and over again; and it also comes in the form of a one-time
+          added value on your first deposit. The Company offers these bonus
+          funds. When you fund your account, the Company matches your first real
+          money deposit by a certain amount of percentage in accordance to your
+          first deposit.
+        </li>
+        <li>
+          1. Warning: A trading bonus gives you great value and extra trading
+          leverage. With all of the above being mentioned; when you trade with
+          bonus leverage you need to be cautious. Leverage is a valuable trading
+          tool, but you need to be aware of the downside. You may close higher
+          trades and make more money initially (or in the long run), but you can
+          also lose a lot more money. Option trading can be risky and you need
+          to trade with confidence and responsibility to avoid losses.
+        </li>
+        <li>
+          2. Scope of Trading Bonus at AssetxPro ™: To help traders starting out
+          CFD trading online, the Company offers new depositing clients a
+          trading bonus on their first deposit as well as redeposit bonuses.
+          There are several types of trading bonuses offered by the Company, as
+          follows:
+        </li>
+        <li>Types of Trading Bonuses</li>
+        <li>
+          After receiving the sign up bonus, traders are given several other
+          opportunities to boost their account balance. The Company often offers
+          a “broker bonus” before significant financial events to enable traders
+          to take advantage of the market volatility around those times. The
+          Company also offers “no deposit bonus” for VIP account holders. These
+          “no deposit bonuses” are called “rebate bonuses”, since they are based
+          on a percentage of the trader’s trading volume. A rebate bonus is
+          usually given on a monthly basis and it’s the Company’s way of giving
+          back a percentage of the traders’ investment that month. A “risk free
+          trade” is also categorized as a bonus as per this agreement and may be
+          offered by the company at its sole discretion.
+        </li>
+        <li>
+          3. The Company also offers special trading bonus offers for traders
+          depositing with certain funding methods, such as, wire transfer, and
+          others. These bonuses can change so you should always check before
+          depositing which funding method will give you the best bonus.
+        </li>
+        <li>
+          4. PLEASE READ ALL TERMS & CONDITIONS BELOW BEFORE ACCEPTING A BONUS.
+          YOU ARE NOT REQUIRED TO ACCEPT A BONUS. A TRADING BONUS IS OPTIONAL
+          FOR ALL CLIENTS.
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default BonusPolicy;
---
+++ src/componenets/Home/PrivacyPolicy.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+function PrivacyPolicy() {
+  return (
+    <div className="w-full flex flex-col gap-8 justify-start items-center md:items-start">
+      <p className="text-3xl font-extrabold">PRIVACY POLICY</p>
+      <div>
+        <p>
+          We are using security measures and encryption systems, to make sure
+          interaction with our website is smooth and safe. All transactions are
+          performed under secured protocols, using the username and password
+          chosen and guarded by you; the chances of someone else trading in your
+          name are extremely low..
+        </p>
+        <p>
+          Keeping your password safe: You should make sure that you don’t share
+          the password with anyone and remember that AssetxPro ™ ’s personnel
+          will never ask you for the password. If this happens and someone
+          claims to be working for AssetxPro ™, don’t provide them with any
+          information and make sure you notify our customer service at once.
+          Pick a password that is hard to intercept and change it every couple
+          of months. Also remember to log out as soon as you stop trading with
+          AssetxPro ™, whenever you leave the computer and after every session.
+          The first step to start trading is to open an account and this
+          process, simple as it is, requires you to enter some personal data
+          such as your first and last name, email and phone number. Additional
+          details, as well as proof of identity may be required of you at any
+          time. AssetxProl ™ will maintain the privacy of all your personal data
+          we collect, including age, occupation or marital status. You may be
+          required to provide the copies of ID, passport and photograph, in
+          accordance with any policies, laws or regulations that require such
+          documents to be delivered. Furthermore, in some jurisdiction you may
+          also need to submit financial information, investment experience and
+          risk tolerance, all as may be required under law.
+        </p>
+      </div>
+      <div>
+        <p>
+          In order to have unrestricted access to the trading account and be
+          able to trade with ease, these documents and information may be
+          required. There is no obligation on your part to submit them, but if
+          you don’t, you won’t be able to open the account and trade online.
+          Additionally, it is important to keep us informed about your personal
+          data as soon as something important changes, so your profile will
+          always be up to date, and as required under any law.
+        </p>
+        <p>
+          We collect this information to administer and operate services,
+          prevent fraud, defend our legal rights and for the purpose of
+          compliance with regulations regarding the services we provide.
+          AssetxPro ™’s privacy policy guarantees that your personal information
+          will not be sold, rented or traded, and won’t be disclosed unless the
+          following conditions occur or for the purposes set out above. We share
+          information with the associated firms and other persons processing
+          your personal information or with banks and clearing houses in order
+          to deliver the products and services you need for trading.
+        </p>
+        <p>
+          When there is any suspicion of fraud or breach regarding the trading
+          performed by the user, the privacy policy allows us to disclose your
+          private data as needed, including to credit reference and fraud
+          prevention agencies. The same applies for the national an
+          international enforcement bodies and courts when they require this
+          kind of information, as laws and regulations demand. Except for these
+          situations, while you are trading with AssetxPro ™, your personal
+          information is reasonably safe from unauthorized use, damage,
+          modification or disclosure, by all reasonable measures.
+        </p>
+      </div>
+      <div>
+        <p>Changes to This Privacy Policy</p>
+        <p>
+          This privacy policy may be changed from time to time, as we see fit.
+          The binding version is the most updated version posted on the site. We
+          may, at our discretion, inform you via e-mail regarding any material
+          changes.
+        </p>
+      </div>
+      <div>
+        <p>
+          Please note any and all usage of the site and services is subject to
+          the Terms and Conditions, as may be amended from time to time by
+          AssetxPro ™, at its sole discretion.
+        </p>
+        <p>
+          Contact Us For queries concerning policy matters, please contact us at
+          anytime.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
